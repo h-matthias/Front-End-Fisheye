cfa71028643a0dcf3fb62d8821612e6e6585b3ba
Hoarau Matthias <h.matthias@live.fr>
2022-12-05 12:57:36 +0400

    import data and integrate homepage

--- scripts/factories/photographer.js
+++ scripts/factories/photographer.js
@@ -1,17 +1,48 @@
 function photographerFactory(data) {
-    const { name, portrait } = data;
+	const {id, name, portrait, city, country, tagline, price } = data
 
-    const picture = `assets/photographers/${portrait}`;
+	const picture = `assets/photographers/${portrait}`
 
-    function getUserCardDOM() {
-        const article = document.createElement( 'article' );
-        const img = document.createElement( 'img' );
-        img.setAttribute("src", picture)
-        const h2 = document.createElement( 'h2' );
-        h2.textContent = name;
-        article.appendChild(img);
-        article.appendChild(h2);
-        return (article);
-    }
-    return { name, picture, getUserCardDOM }
+	function getUserCardDOM() {
+		const article = document.createElement('article')
+		const a = document.createElement('a')
+		a.setAttribute('href', '#')
+		const img = document.createElement('img')
+		img.setAttribute('src', picture)
+		img.setAttribute('alt', '')
+		const h2 = document.createElement('h2')
+		h2.textContent = name
+		a.appendChild(img)
+		a.appendChild(h2)
+		article.appendChild(a)
+
+		const aside = document.createElement('aside')
+		const cityAndCountry = document.createElement('p')
+		const tag = document.createElement('p')
+		const priceElt = document.createElement('p')
+
+		cityAndCountry.textContent = `${city}, ${country}`
+		cityAndCountry.classList.add('city')
+		tag.textContent = tagline
+		tag.classList.add('tagline')
+        priceElt.textContent = `${price}€/jour`
+		priceElt.classList.add('price')
+        aside.appendChild(cityAndCountry)
+        aside.appendChild(tag)
+        aside.appendChild(priceElt)
+
+        article.appendChild(aside)
+
+		return article
+	}
+	return {
+		id,
+		name,
+		city,
+		country,
+		tagline,
+		price,
+		picture,
+		getUserCardDOM,
+	}
 }
